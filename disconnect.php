723d4a1a594109bd759de6bc582b968355550a6a
Marmouz82 <gtn.massart@gmail.com>
2020-11-25 16:38:14 +0100

    last update gaetan

---
+++ disconnect.php
@@ -0,0 +1,18 @@
+<?php
+
+include "includes/connect.php";
+
+
+    // Inialize session
+    session_start();
+
+    // Delete certain session
+    //unset($_SESSION['pseudo']);
+    // Delete all session variables
+    session_destroy();
+
+    // Jump to login page
+    header('Location: index.php');
+        
+?>
+
