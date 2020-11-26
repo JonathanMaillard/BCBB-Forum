b4b32e57301ed3dba61f97f26c45a1d3dd7f41b5
Marmouz82 <gtn.massart@gmail.com>
2020-11-26 14:28:38 +0100

    last update gaetan

---
+++ profile.php
@@ -0,0 +1,61 @@
+<?php
+    session_start();
+
+    $arianne = '<a href=#><i class="fa fa-home" aria-hidden="true"></i> Home > Profile</a>';
+    $titre = "Profile - Rolling Stones Forum";
+    $css = 'style';
+
+
+    include "includes/connect.php";   
+    include "includes/header.php";
+
+    if(!isset($_SESSION['id'])) {
+
+        echo "<h2> You must be logged to access this page... GET OUT!!!</h2>";
+        include "includes/footer.php";
+        exit();
+    }
+
+
+
+?>
+
+<h2>Profile</h2>
+
+<form class="form" action="target_profile.php" method="POST">
+
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            <label for="inputUsername">Username</label>
+            <input type="text" class="form-control" id="inputUsername" name="inputUsername" placeholder="Enter your name">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="inputAvatar">Avatar</label>
+            <input type="text" class="form-control" id="inputAvatar" name="inputAvatar" placeholder="Enter your Avatar">
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            <label for="inputUserSignature">Signature</label>
+            <input type="text" class="form-control" id="inputUserSignature" name="inputUserSignature" placeholder="Enter your Signature">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="inputPassword">Password</label>
+            <input type="password" class="form-control" id="inputPassword" name="inputPassword" placeholder="Enter your Password">
+        </div>
+        
+       <div class="form-group col-md-6">
+            <label for="inputPassword2">Password*</label>
+            <input type="password" class="form-control" id="inputPassword2" name="inputPassword2" placeholder="Re-enter your Password">
+        </div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+        
+</form>
+
+
+<?php 
+    include "includes/footer.php";
+?>
---
+++ target_profile.php
@@ -0,0 +1,61 @@
+<?php 
+    session_start();
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $arianne = '<p><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a> > Profile</p>';
+    $titre = "Profile - Rolling Stones Forum";
+?>
+
+<?php
+
+include "includes/connect.php";
+include "includes/header.php";
+ 
+if(isset($_SESSION['id'])) {
+
+   $req = $db->prepare("SELECT * FROM users WHERE user_id = ". $_SESSION['id'] ."");
+   $req->execute();
+   $user = $req->fetch();
+   
+
+   if(isset($_POST['inputUsername']) AND !empty($_POST['inputUsername']) AND $_POST['inputUsername'] != $user['user_name']) {
+
+        $newInputUsername = htmlspecialchars($_POST['inputUsername']);
+        $insertUsername = $db->prepare("UPDATE users SET user_name = ? WHERE user_id = ?");
+        $insertUsername->execute(array($newInputUsername, $_SESSION['id']));
+        $_SESSION['pseudo'] = $newInputUsername;
+    }
+
+    if(isset($_POST['inputUserSignature']) AND !empty($_POST['inputUserSignature']) AND $_POST['inputUserSignature'] != $user['user_signature']) {
+        $newInputUserSignature = htmlspecialchars($_POST['inputUserSignature']);
+        $insertSignature = $db->prepare("UPDATE users SET user_signature = ? WHERE user_id = ?");
+        $insertSignature->execute(array($newInputUserSignature, $_SESSION['id']));
+    }
+
+    if(isset($_POST['inputAvatar']) AND !empty($_POST['inputAvatar']) AND $_POST['inputAvatar'] != $user['user_avatar']) {
+        $newInputAvatar = htmlspecialchars($_POST['inputAvatar']);
+        $insertInputAvatar = $db->prepare("UPDATE users SET user_avatar = ? WHERE user_id = ?");
+        $insertInputAvatar->execute(array($newInputAvatar, $_SESSION['id']));
+    } 
+
+    if(isset($_POST['inputPassword']) AND !empty($_POST['inputPassword']) AND isset($_POST['inputPassword2']) AND !empty($_POST['inputPassword2'])) {
+        $password1 = hash('sha256', $_POST['inputPassword']);
+        $password2 = hash('sha256', $_POST['inputPassword2']);
+
+        if($password1 == $password2) {
+            $insertmdp = $db->prepare("UPDATE users SET user_pass = ? WHERE user_id = ?");
+            $insertmdp->execute(array($password1, $_SESSION['id']));
+            
+        } else {
+            $msg = "Vos deux mdp ne correspondent pas !";
+        }
+    }
+ }
+?>
+
+
+<?php 
+    include "includes/footer.php";
+?>
