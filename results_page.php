fe7096610c50779b03ccd43b1bfd06d0ae91fcc5
KevKsar <cassart.kev@gmail.com>
2020-12-04 14:58:45 +0100

    results_page inDev

--- results_page.php
+++ results_page.php
@@ -13,22 +13,22 @@
 
 <div class="row container-fluid">
     <div class="col">
-        <h2>Search results for <em><?php echo $search; ?></em> </h2>
+        <h2>Search results for <em><?php echo $search;?></em> </h2>
     </div>
 </div>
 
 <div class="container-fluid bg-light rounded-lg m-2 pb-1">
     <div class="banner-aside row d-flex align-items-center">
-        <div class="card-header__element col-7">
+        <div class="card-header__element col-6">
             <p class="h6 mb-1 ">Topics Results</p>
         </div>
-        <div class="content-header__element col-1">
+        <div class="content-header__element col-2 text-center">
             <i class="fa fa-comments-o" aria-hidden="true"></i>
         </div>
-        <div class="content-header__element col-1">
+        <div class="content-header__element col-2 text-center">
             <i class="fa fa-eye" aria-hidden="true"></i>
         </div>
-        <div class="content-header__element col-2">
+        <div class="content-header__element col-2 text-center">
             <i class="fa fa-clock-o" aria-hidden="true"></i>
         </div>
     </div>
@@ -37,22 +37,21 @@
 
 <div class="container-fluid bg-light rounded-lg m-2 pb-1">
     <div class="banner-aside row d-flex align-items-center">
-        <div class="card-header__element col-7">
+        <div class="card-header__element col-6">
             <p class="h6 mb-1 ">Posts Results</p>
         </div>
-        <div class="content-header__element col-1">
-            <i class="fa fa-comments-o" aria-hidden="true"></i>
+        <div class="content-header__element col-2 text-center">
+            <!-- <i class="fa fa-comments-o" aria-hidden="true"></i> -->
         </div>
-        <div class="content-header__element col-1">
+        <div class="content-header__element col-2 text-center">
             <i class="fa fa-eye" aria-hidden="true"></i>
         </div>
-        <div class="content-header__element col-2">
+        <div class="content-header__element col-2 text-center">
             <i class="fa fa-clock-o" aria-hidden="true"></i>
         </div>
     </div>
     <?php include 'includes/posts_results.php';?>
 </div>
-
 
 <?php
     include('includes/footer.php');
