2c8aaa75cb50f004b042bdf328840d0514c85420
KevKsar <cassart.kev@gmail.com>
2020-12-08 11:38:20 +0100

    feat stats > categories InDev

--- index.php
+++ index.php
@@ -27,21 +27,23 @@
 
 <?php
     //Get categories infos
-    $query=$db->prepare('SELECT cat_id, cat_name, cat_description FROM categories');
+    $query=$db->prepare('SELECT cat_id, cat_name, cat_description 
+    FROM categories');
 
     $query->execute();
 
     //Loop on each catgory
     while($data = $query->fetch())
-    { ?>
-        <!-- // Echo of categories infos
-        echo  -->
-            <div class="category">
+    {
+
+        // Echo of categories infos
+        echo 
+            '<div class="category">
 
                 <div class="container">
                     <div class="row category__start">
-                        <div class="col-3">
-                        <?php
+                        <div class="col-3">';
+
                         if($data['cat_id'] == 1){   
                             echo "<img class='category__img' src='./images/guitar.png'>";
                         }
@@ -62,58 +64,49 @@
                         }
                         else{
                             echo "";
-                        }?>
-                        <!-- // ??? INTERCALER ICI LE CODE POUR LA MYSTERY PAGE ??? -->
-                        </div>
+                        }
+                        // ??? INTERCALER ICI LE CODE POUR LA MYSTERY PAGE ???
+                        echo '</div>
                         <div class="col-9">
-                            <h4 class="category__title"><a href="./topics.php?id=<?php $data['cat_id'];?>"><?php echo stripslashes(htmlspecialchars($data['cat_name']));?></a></h4>
-                            <p class="category__description"> <?php echo stripslashes(htmlspecialchars($data['cat_description']));?></p>
+                            <h4 class="category__title"><a href="./topics.php?id='.$data['cat_id'].'">'.stripslashes(htmlspecialchars($data['cat_name'])).'</a></h4>
+                            <p class="category__description">'.stripslashes(htmlspecialchars($data['cat_description'])).'</p>
                         </div>
                     </div>
                     <div class ="row category__end">
                         <div class="col-3 bordered">
-                            <p class="category__numbers">  
-                                <?php
-                                $numOfTopics = $db->query('SELECT COUNT(*) AS topic_cat FROM topics WHERE topic_cat =' . $data['cat_id']);
+                            <p class="category__numbers">';
+                                $numOfTopics = $db->query("SELECT COUNT(*) AS topic_cat FROM topics WHERE topic_cat =" . $data['cat_id']);
                                 while($topics = $numOfTopics->fetch()){
-                                echo $topics['topic_cat'];            
-                                }
+                                    echo $topics["topic_cat"];            
+                                } 
                                 $numOfTopics->CloseCursor();
-                                ?>
-                            </p>
+                            echo'</p>      
                             <p class="category__text">Topics</p>
                         </div>
                         <div class="col-3">
-                            <p class="category__numbers">
-                                <?php
-                                $postInCat = $db->query('SELECT COUNT(*) AS topic_cat FROM topics WHERE topic_cat =' . $data['cat_id']);
-                                while($posts = $postInCat->fetch()){
-                                echo $posts['topic_cat'];            
-                                }
-                                $postInCat->CloseCursor();
-                                ?>                           
-                            </p>
+                            <p class="category__numbers">15</p>
                             <p class="category__text">Posts</p>
                         </div>
                         <div class="col-6">
                             <p class="category__date">Fri Nov 27</p>
-                            <p class="category__text">Last post</p>
+                            <p class="category__text">Last  post</p>
                         </div>
                     </div>
                 </div>
-            </div>
-        
+            </div>';
+        //;
 
-        
-        <!-- <tr><td><img src="./images/message.gif" alt="message" /></td>
+        /*
+        echo'<tr><td><img src="./images/message.gif" alt="message" /></td>
         <td class="titre">
-        <a href="./topic.php?f=<? $data['cat_id'] ?>">
-        <? stripslashes(htmlspecialchars($data['cat_name']))?></a>
-        <br /> <?nl2br(stripslashes(htmlspecialchars($data['cat_description'])))?></td>
-        <td class="nombresujets"><? $data['forum_topic']?></td>
-        <td class="nombremessages"><?$data['forum_post']?></td> -->
-        
-    <?} 
+        <a href="./topic.php?f='.$data['cat_id'].'">
+        '.stripslashes(htmlspecialchars($data['cat_name'])).'</a>
+        <br />'.nl2br(stripslashes(htmlspecialchars($data['cat_description']))).'</td>
+        <td class="nombresujets">'.$data['forum_topic'].'</td>
+        <td class="nombremessages">'.$data['forum_post'].'</td>';
+        */
+
+    } //loop end
     
     $query->CloseCursor();
 ?>
