fe13e0db91db8e535301534b1108f646b8244911
KevKsar <cassart.kev@gmail.com>
2020-12-14 13:52:01 +0100

    upd

--- comment.php
+++ comment.php
@@ -39,7 +39,7 @@
     
     <div class="main">
         <!-- Div2 à droite (contient chemin, titre, forum rules, trois boutons (reply, tools et search) + "1 post page 1/1" et totalité des commentaires) -->
-        <div class="mainRight">   
+        <div class="mainRight container">   
 
             <?php
 
@@ -92,85 +92,88 @@
 
         $query->execute(); ?>
 
-        <?php while($data = $query->fetch()) { ?>
+        <div class="container-fluid bg-light rounded-lg m-2 py-2">
+            <?php while($data = $query->fetch()) { ?>
 
-        <div class="mainRightComment">
-            <div class="container bg-light rounded-lg m-2">
-                    <div class="commentInside">
-                        <div class="detailUser">
-                        <?php $req_avatar = $db->query("SELECT user_id, user_avatar FROM users WHERE user_id =". $data['post_by']);
-                                while($avatar = $req_avatar->fetch()){ ?>
-                                    <img id="avatar" src="<?php echo($avatar["user_avatar"]); ?>">
-                                <?php } ?>
-                                    
+            
+            <div class="mainRightComment">
+                <div class="container-fuide ">
+                        <div class="commentInside">
+                            <div class="detailUser">
+                            <?php $req_avatar = $db->query("SELECT user_id, user_avatar FROM users WHERE user_id =". $data['post_by']);
+                                    while($avatar = $req_avatar->fetch()){ ?>
+                                        <img id="avatar" src="<?php echo($avatar["user_avatar"]); ?>">
+                                    <?php } ?>
+                                        
 
-                            <!-- <img id="avatar" src="" alt=""> -->
-                        
-                            <label id="userName">
-                                <?php $req_user = $db->query("SELECT user_id, user_name FROM users WHERE user_id =". $data['post_by']);
-                                while($user = $req_user->fetch()) { ?>
-                                <a href="#"> 
-                                    <strong>
-                                        <?php 
-                                            echo $user['user_name'];
-                                }
-                                            $req_user->closeCursor();
-                                        ?>
-                                    </strong>
-                                </a>
-                            </label>
-                            <label id="postNumber">
-                                <?php $req_total = $db-> query('SELECT COUNT(post_id) AS numberPosts FROM posts WHERE post_by = '. $data['post_by']);
-                                while($total = $req_total->fetch()) { ?>
+                                <!-- <img id="avatar" src="" alt=""> -->
+                            
+                                <label id="userName">
+                                    <?php $req_user = $db->query("SELECT user_id, user_name FROM users WHERE user_id =". $data['post_by']);
+                                    while($user = $req_user->fetch()) { ?>
                                     <a href="#"> 
                                         <strong>
                                             <?php 
-                                                echo 'Posts : ' . $total['numberPosts'];
+                                                echo $user['user_name'];
                                     }
-                                                $req_total->closeCursor();
+                                                $req_user->closeCursor();
                                             ?>
                                         </strong>
                                     </a>
-                    
-                            </label>
-                            <label id="location">Location</label>  
-                        </div>
-                    
-                        <div class="detailMessage">
-
-                            <label class= "date"><?php echo $data['post_date'] ?></label>
-                            <div class="commentContent">
-                                <?php 
-                                    $emoji_replace = array(":)", ":-)", ":smile:", ">:(", ">:-(", ":angry:", "<3", ":love:", ":'", ":'(", ":cry:", ":D", ":-D", ":lol:", ";)", ";-)", ":wink:", "8)", "8-)", ":nerd:", ":(", ":-(", ":sad:" );
-                                    $emoji_new = array('<img src="emojis/emo_smile.png"/>', '<img src="emojis/emo_smile.png">', '<img src="emojis/emo_smile.png">', '<img src="emojis/emo_angry.png">', '<img src="emojis/emo_angry.png">',
-                                                        '<img src="emojis/emo_angry.png">', '<img src="emojis/emo_love.png">', '<img src="emojis/emo_love.png">', '<img src="emojis/emo_cry.png">',
-                                                        '<img src="emojis/emo_cry.png">', '<img src="emojis/emo_cry.png">', '<img src="emojis/emo_lol.png">', '<img src="emojis/emo_lol.png">', '<img src="emojis/emo_lol.png">',
-                                                        '<img src="emojis/emo_wink.png">', '<img src="emojis/emo_wink.png">', '<img src="emojis/emo_wink.png">','<img src="emojis/emo_nerd.png">', '<img src="emojis/emo_nerd.png">',
-                                                        '<img src="emojis/emo_nerd.png">', '<img src="emojis/emo_sad.png">', '<img src="emojis/emo_sad.png">', '<img src="emojis/emo_sad.png">' );
-                                    $emojis = str_replace($emoji_replace, $emoji_new, $data['post_content']); 
-                                ?>
-                                <div  class="post-content" id="textarea" cols= "70" rows="5"><?php echo $emojis ?></div>
+                                </label>
+                                <label id="postNumber">
+                                    <?php $req_total = $db-> query('SELECT COUNT(post_id) AS numberPosts FROM posts WHERE post_by = '. $data['post_by']);
+                                    while($total = $req_total->fetch()) { ?>
+                                        <a href="#"> 
+                                            <strong>
+                                                <?php 
+                                                    echo 'Posts : ' . $total['numberPosts'];
+                                        }
+                                                    $req_total->closeCursor();
+                                                ?>
+                                            </strong>
+                                        </a>
+                        
+                                </label>
+                                <label id="location">Location</label>  
                             </div>
-                    
-                            <label class= "signature">
-                                <?php $req_user = $db->query("SELECT user_id, user_signature FROM users WHERE user_id =". $data['post_by']);
-                                while($user = $req_user->fetch()) { ?>
-                                <a href="#"> 
-                                    <strong>
-                                        <?php 
-                                            echo $user['user_signature'];
-                                }
-                                            $req_user->closeCursor();
-                                        ?>
-                                    </strong>
-                                </a>
-                            </label>
-                        </div> 
+                        
+                            <div class="detailMessage">
+
+                                <label class= "date"><?php echo $data['post_date'] ?></label>
+                                <div class="commentContent">
+                                    <?php 
+                                        $emoji_replace = array(":)", ":-)", ":smile:", ">:(", ">:-(", ":angry:", "<3", ":love:", ":'", ":'(", ":cry:", ":D", ":-D", ":lol:", ";)", ";-)", ":wink:", "8)", "8-)", ":nerd:", ":(", ":-(", ":sad:" );
+                                        $emoji_new = array('<img src="emojis/emo_smile.png"/>', '<img src="emojis/emo_smile.png">', '<img src="emojis/emo_smile.png">', '<img src="emojis/emo_angry.png">', '<img src="emojis/emo_angry.png">',
+                                                            '<img src="emojis/emo_angry.png">', '<img src="emojis/emo_love.png">', '<img src="emojis/emo_love.png">', '<img src="emojis/emo_cry.png">',
+                                                            '<img src="emojis/emo_cry.png">', '<img src="emojis/emo_cry.png">', '<img src="emojis/emo_lol.png">', '<img src="emojis/emo_lol.png">', '<img src="emojis/emo_lol.png">',
+                                                            '<img src="emojis/emo_wink.png">', '<img src="emojis/emo_wink.png">', '<img src="emojis/emo_wink.png">','<img src="emojis/emo_nerd.png">', '<img src="emojis/emo_nerd.png">',
+                                                            '<img src="emojis/emo_nerd.png">', '<img src="emojis/emo_sad.png">', '<img src="emojis/emo_sad.png">', '<img src="emojis/emo_sad.png">' );
+                                        $emojis = str_replace($emoji_replace, $emoji_new, $data['post_content']); 
+                                    ?>
+                                    <div  class="post-content" id="textarea" cols= "70" rows="5"><?php echo $emojis ?></div>
+                                </div>
+                        
+                                <label class= "signature">
+                                    <?php $req_user = $db->query("SELECT user_id, user_signature FROM users WHERE user_id =". $data['post_by']);
+                                    while($user = $req_user->fetch()) { ?>
+                                    <a href="#"> 
+                                        <strong>
+                                            <?php 
+                                                echo $user['user_signature'];
+                                    }
+                                                $req_user->closeCursor();
+                                            ?>
+                                        </strong>
+                                    </a>
+                                </label>
+                            </div> 
+                        </div>
+                        
                     </div>
-                    
                 </div>
-            </div>
-            <?php } ?>
+                <?php } ?>
+        </div>
         
 
             <a href="post_message.php?topic_id=<?php echo $topic_id;?>" type="button" class="btn btn-primary">Post Reply <i class="fa fa-reply" aria-hidden="true"></i></a>  
