c9343efa121d3a8afe7e9421006d6ee62d003548
KevKsar <cassart.kev@gmail.com>
2020-12-14 11:43:55 +0100

    udp search

--- includes/req_search.php
+++ includes/req_search.php
@@ -5,6 +5,11 @@
         $search = htmlspecialchars($_GET['search']);
         $req_topics_results = $db->query('SELECT * FROM topics WHERE topic_search = 0 AND topic_subject LIKE "%'.$search.'%"');
         $req_posts_results = $db->query('SELECT * FROM posts WHERE post_search = 0 AND post_content LIKE "%'.$search.'%"');
+        
+        $countTopicResults=$req_topics_results->rowCount();
+        $countPostResults=$req_posts_results->rowCount();
+        $countTotalResult= $countTopicResults + $countPostResults;
+
         $search_done = true;
     }
 ?>
